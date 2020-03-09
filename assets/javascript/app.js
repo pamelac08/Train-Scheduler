49097d629286873c766375681fa0025cad89f0ce
Pamela Christian <pamelac08@gmail.com>
2020-03-09 09:32:41 -0500

    functioning app that updates current time and timetable elements each minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,31 @@
+//this version is the app functioning with minimum requirements: 
+    //create a train schedule application that incorporates Firebase to host arrival and departure data.
+  //Your app will retrieve and manipulate this information with Moment.js
+  //This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station
+
+// Make sure that your app suits this basic spec:
+  // When adding trains, administrators should be able to submit the following:
+      // Train Name
+      // Destination
+      // First Train Time -- in military time
+      // Frequency -- in minutes
+  // Code this app to calculate when the next train will arrive; this should be relative to the current time.
+  // Users from many different machines must be able to view same train times.
+  // Styling and theme are completely up to you. Get Creative!
+
+
+// Plus the following bonus tasks:
+    // 1. includes the local current time displayed on the train timetable
+    // 2. updates the timetable "minutes to arrival" and "next train time" text once every minute. 
+
+
+// next iteration to do:
+    // put current time into a div and display in same header as "Current Train Schedule"
+    // allow user to "remove" train schedule with "x" button
+
+
+$(document).ready(function () {
+
 //initialize firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyAYSCFZLQJkQypBEHOHA60Nz1cIoAvh8aE",
@@ -13,9 +41,17 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-//display current time
+
+//display current time initially
 var currentTimeFormatted = moment().format('LT');
 $("#current-time").text("Current Time: " + currentTimeFormatted);
+
+//every 5 seconds, the current time is checked 
+//if it has changed, the current time on the page is updated
+setInterval(updateCurrentTime, 5000);
+
+//display database initially
+updateTimetable();
 
 
 // Button for adding train schedules
@@ -25,7 +61,6 @@ $("#add-train").on("click", function(event) {
     // Grabs user input
     var trainName = $("#input-name").val().trim();
     var destination = $("#input-destination").val().trim();
-
     var hour =  $("#input-first-time-hour").val().trim();
     var minutes =  $("#input-first-time-minute").val().trim();
     var sHour = hour.toString();
@@ -33,24 +68,19 @@ $("#add-train").on("click", function(event) {
     var firstTrainTime = (sHour + ":" + sMinutes);
     var frequency = $("#input-frequency").val().trim();
   
-    // Creates local "temporary" object for holding employee data
+    // Creates local "temporary" object for holding train data
     var newTrain = {
       name: trainName,
       destination: destination,
       firstTrainTime: firstTrainTime,
-      frequency: frequency
+      frequency: frequency,
+      currentTimeFormatted: currentTimeFormatted
     };
   
-    // Uploads employee data to the database
+    // Uploads train data to the database
     database.ref().push(newTrain);
   
-    // Logs everything to console
-    // console.log(newTrain.name);
-    // console.log(newTrain.destination);
-    // console.log(newTrain.firstTrainTime);
-    // console.log(newTrain.frequency);
-  
-    alert("Train successfully added");
+    alert("Train schedule successfully added");  
   
     // Clears all of the text-boxes
     $("#input-name").val("");
@@ -58,97 +88,81 @@ $("#add-train").on("click", function(event) {
     $("#input-first-time-hour").val("");
     $("#input-first-time-minute").val("");
     $("#input-frequency").val("");
+
+    updateTimetable();
   });
 
+/////////// Functions //////////////////////
 
-  // Create Firebase event for adding train schedule to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
-    // console.log(childSnapshot.val());
-  
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().name;
-    var destination= childSnapshot.val().destination;
-    var firstTrainTime = childSnapshot.val().firstTrainTime;
-    var frequency = childSnapshot.val().frequency;
-  
-    // Train Info
-    // console.log(trainName);
-    // console.log(destination);
-    // console.log(firstTrainTime);
-    // console.log(frequency);
+//updating time and timetable every 5 seconds
+function updateCurrentTime() {
 
-
-    //calculate next arrival
-
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
-    // console.log(firstTimeConverted);
-
-    // Current Time
-    // var currentTime = moment();
-    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    // console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % frequency;
-    // console.log(tRemainder);
-
-    // Minute Until Train
-    var tMinutesTillTrain = frequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format('LT'));
-    var nextTrainFormatted = moment(nextTrain).format('LT');
+    var newCurrentTimeFormatted = moment().format('LT');
+    $("#current-time").text("Current Time: " + newCurrentTimeFormatted);
     
-    // Create the new row
-    var newRow = $("<tr>").append(
-      $("<td>").text(trainName),
-      $("<td>").text(destination),
-      $("<td>").text(frequency),
-      $("<td>").text(nextTrainFormatted),
-      $("<td>").text(tMinutesTillTrain),
-    );
-    // Append the new row to the table
-    $("tbody").append(newRow);
-  });
+    if (currentTimeFormatted !== newCurrentTimeFormatted) {
+        currentTimeFormatted = newCurrentTimeFormatted;
+        updateTimetable();
+    }
+}
+
+//run inside updateTimeTable function to clear rows in timetable before re-rendering them
+function DeleteRows() {
+    var rowCount = currentSchedule.rows.length;
+    for (var i = rowCount - 1; i > 0; i--) {
+        currentSchedule.deleteRow(i);
+    }
+}
+
+//to update timetable every minute or when new train schedules are added
+function updateTimetable() {
+
+    DeleteRows();
 
 
-//update page every minute with new arrival time and minutes away
-//an event would happen at the top of each minute
-  //event listener 
-
-
-
-
-
-
-// setInterval(checkTime, 5000);
-
-// function checkTime() {
-
-//     var currentTime = moment().format('LT');
-
-//     if (currentTime !== currentTime) {
-//         currentTimeFormatted = currentTime;
-//         $("#current-time").text("Current Time: " + currentTimeFormatted);
-//     }
-//     console.log(currentTime);
-// }
-
-
-
+    database.ref().on("child_added", function(childSnapshot) {
+    
+        // Store everything into a variable.
+        var trainName = childSnapshot.val().name;
+        var destination= childSnapshot.val().destination;
+        var firstTrainTime = childSnapshot.val().firstTrainTime;
+        var frequency = childSnapshot.val().frequency;
   
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % frequency;
+
+        // Minute Until Train
+        var tMinutesTillTrain = frequency - tRemainder;
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        var nextTrainFormatted = moment(nextTrain).format('LT');
+    
+        // Create the new row
+        var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td id='next-train'>").text(nextTrainFormatted),
+        $("<td id='minutes-away'>").text(tMinutesTillTrain),
+        );
+
+        // Append the new row to the table
+        $("tbody").append(newRow);
+    });
+}
+
+});
+
 
 
 // Bonus (Extra Challenges)
-// Consider updating your "minutes to arrival" and "next train time" text once every minute. 
-//This is significantly more challenging; only attempt this if you've completed the actual 
-//activity and committed it somewhere on GitHub for safekeeping (and maybe create a second GitHub repo).
 
 // Try adding update and remove buttons for each train. Let the user edit the row's 
 //elements-- allow them to change a train's Name, Destination and Arrival Time (and then, 
